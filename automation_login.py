bf281e49fdc5670ba1d423869db37e4efb888039
barru <barru@motionpay.id>
2022-07-26 19:10:42 +0700

    updated element

--- automation_login.py
+++ automation_login.py
@@ -1,10 +1,7 @@
 import unittest
 import time
 from selenium import webdriver 
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 class TestLogin(unittest.TestCase): 
@@ -17,15 +14,15 @@ class TestLogin(unittest.TestCase):
         driver = self.driver
         driver.get("http://barru.pythonanywhere.com/daftar") # buka situs
         time.sleep(3)
-        driver.find_element_by_id("email").send_keys("tester@jagoqa.com") # isi email
+        driver.find_element(By.ID,"email").send_keys("tester@jagoqa.com") # isi email
         time.sleep(1)
-        driver.find_element_by_id("password").send_keys("testerjago") # isi password
+        driver.find_element(By.ID,"password").send_keys("testerjago") # isi password
         time.sleep(1)
-        driver.find_element_by_id("signin_login").click()
+        driver.find_element(By.ID,"signin_login").click()
         time.sleep(1)
 
-        response_data = driver.find_element_by_id("swal2-title").text
-        response_message = driver.find_element_by_id("swal2-content").text
+        response_data = driver.find_element(By.ID,"swal2-title").text
+        response_message = driver.find_element(By.ID,"swal2-content").text
 
         self.assertEqual(response_data, 'Welcome tester jago')
         self.assertEqual(response_message, 'Anda Berhasil Login')
